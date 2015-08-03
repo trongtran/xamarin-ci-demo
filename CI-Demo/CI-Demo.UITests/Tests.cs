2eaf6c99c0456a55a35a53f97c1d42d2104df336
Trong Tran <trongtran.javadev@gmail.com>
2015-08-03 10:28:09 +0700

    push something for running unit testing via Jenkins

--- CI-Demo/CI-Demo.UITests/Tests.cs
+++ CI-Demo/CI-Demo.UITests/Tests.cs
@@ -16,13 +16,23 @@ namespace CIDemo.UITests
 		[SetUp]
 		public void BeforeEachTest ()
 		{
-			app = ConfigureApp.iOS.StartApp ();
+			app = ConfigureApp
+				.iOS
+				// TODO: Update this path to point to your iOS app and uncomment the
+				// code if the app is not included in the solution.
+				.AppBundle ("../../../CI-Demo/bin/iPhoneSimulator/Debug/CI-Demo.app")
+				.StartApp ();
 		}
 
 		[Test]
 		public void ViewIsDisplayed ()
 		{
 			AppResult[] results = app.WaitForElement (c => c.Child ("UIView"));
+
+			// Tap on navigation bar button
+			app.Tap(x=>x.Id("page1Button")); 
+
+
 			app.Screenshot ("First screen.");
 
 			Assert.IsTrue (results.Any ());
